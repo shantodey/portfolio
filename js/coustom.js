400c44321a97e1a8656c2b93e917ce5824c9ecd0
Shanto Dey <126257294+shantodey@users.noreply.github.com>
2023-05-17 22:57:30 +0600

    Update coustom.js

--- js/coustom.js
+++ js/coustom.js
@@ -1,26 +1,55 @@
 
-$(document).ready(function(){
-        
-    $("#colorful").colorfulTab();    
-    
-    $("#colorful-elliptic").colorfulTab({
-        theme: "elliptic"
-    }); 
-   
-   $("#colorful-flatline").colorfulTab({
-        theme: "flatline"
-    });    
-    
-    
+//   back_to_top///////////////////
 
-});
+$(document).ready(function(){"use strict";
+	
+		//Scroll back to top
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 550;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		
+	});
+
+
+//   back_to_top///////////////////
+
+
+// counter js start
 
 $('.counter').counterUp({
     delay: 10,
     time: 2500
 });
 
-
+// counter js end
 
 // $('.news_silk').slick({
 //     slidesToShow: 3,
@@ -78,51 +107,7 @@ $('.news_silk').slick({
 
 
 
-//   back_to_top///////////////////
 
-(function($) { "use strict";
-
-		// back to top
-	$(document).ready(function(){"use strict";
-	
-		//Scroll back to top
-		
-		var progressPath = document.querySelector('.progress-wrap path');
-		var pathLength = progressPath.getTotalLength();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-		progressPath.style.strokeDashoffset = pathLength;
-		progressPath.getBoundingClientRect();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
-		var updateProgress = function () {
-			var scroll = $(window).scrollTop();
-			var height = $(document).height() - $(window).height();
-			var progress = pathLength - (scroll * pathLength / height);
-			progressPath.style.strokeDashoffset = progress;
-		}
-		updateProgress();
-		$(window).scroll(updateProgress);	
-		var offset = 50;
-		var duration = 550;
-		jQuery(window).on('scroll', function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.progress-wrap').addClass('active-progress');
-			} else {
-				jQuery('.progress-wrap').removeClass('active-progress');
-			}
-		});				
-		jQuery('.progress-wrap').on('click', function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		})
-		
-		
-	});
-	
-})(jQuery); 
-
-//   back_to_top///////////////////
 
 
 // mixet up ////////////////////
